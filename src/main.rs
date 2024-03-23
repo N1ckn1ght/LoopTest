05053e1e8ad18a27ed60112851a92e985b1f3bd8
N1ckn1ght <thisefi@yandex.ru>
2024-03-23 20:15:36 +0800

    indev

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use engine::Engine;
 fn main() {
     let mut engine = Engine::default();
     let mut state = 0;
+    let mut output = false;
 
     /* Required states:
         0 | stop    - Do nothing
@@ -25,46 +26,56 @@ fn main() {
         let mut started = lock.lock().unwrap();
         *started = true;
 
-        let mut input = String::new();
-        match io::stdin().read_line(&mut input) {
-            Ok(_goes_into_input_above) => {
-                let line = input.trim().split(" ").collect::<Vec<&str>>();
-                match line[0] {
-                    "stop" => {
-                        engine.abort = true;
-                        state = 0;
-                        cvar.notify_one();
-                    },
-                    "clear" => {
-                        engine.variable = 0;
-                        state = 1;
+        while state != 5 {
+            let mut input = String::new();
+            match io::stdin().read_line(&mut input) {
+                Ok(_goes_into_input_above) => {
+                    let line = input.trim().split(" ").collect::<Vec<&str>>();
+                    match line[0] {
+                        "stop" => {
+                            engine.abort = true;
+                            output = false;
+                            state = 0;
+                            cvar.notify_one();
+                        },
+                        "clear" => {
+                            engine.abort = true;
+                            output = false;
+                            state = 1;
+                            engine.variable = 0;
+                        }
+                        "start" => {
+                            engine.time_limit_ms = line[1].parse::<u128>().unwrap();
+                            output = true;
+                            state = 2;
+                            cvar.notify_one();
+                        },
+                        "ponder" => {
+                            engine.time_limit_ms = 1 << 63;
+                            output = false;
+                            state = 3;
+                            cvar.notify_one();
+                        },
+                        "now" => {
+                            engine.abort = true;
+                            output = true;
+                            state = 4;
+                            cvar.notify_one();
+                        },
+                        "quit" => {
+                            engine.abort = true;
+                            output = false;
+                            state = 5;
+                            cvar.notify_one();
+                        },
+                        _ => {
+                            println!("Wrong command");
+                        }
                     }
-                    "start" => {
-                        engine.time_limit_ms = line[1].parse::<u128>().unwrap();
-                        state = 2;
-                        cvar.notify_one();
-                    },
-                    "ponder" => {
-                        engine.time_limit_ms = 1 << 63;
-                        state = 3;
-                        cvar.notify_one();
-                    },
-                    "now" => {
-                        engine.abort = true;
-                        state = 4;
-                        cvar.notify_one();
-                    },
-                    "quit" => {
-                        state = 5;
-                        cvar.notify_one();
-                    },
-                    _ => {
-                        println!("Wrong command");
-                    }
+                },
+                Err(_no_updates_is_fine) => {
+                    println!("No updates");
                 }
-            },
-            Err(_no_updates_is_fine) => {
-                println!("No updates");
             }
         }
     });
@@ -78,15 +89,32 @@ fn main() {
 
         match state {
             0 => {
-
+                
             },
             1 => {
-
+                
             },
             2 => {
-
+                let res = engine.calc(engine.time_limit_ms);
+                if output {
+                    println!("Returned variable: {}", res);
+                    output = false;
+                }
             },
             3 => {
+                let res = engine.calc(engine.time_limit_ms);
+                if output {
+                    println!("Pondered variable: {}", res);
+                    output = false;
+                }
+            },
+            4 => {
+                if output {
+                    println!("Current variable: {}", engine.variable);
+                    output = false;
+                }
+            }
+            5 => {
                 println!("Quit signal reached.");
                 break;
             },
